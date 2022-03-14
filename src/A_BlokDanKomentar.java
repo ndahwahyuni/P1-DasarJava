ef11a15d48f110663c1243ba174b9b3a98597b6b
DELL <ndahwahyuni22@gmail.com>
2022-03-14 15:14:17 +0700

    initial project pertemuan 1 dasar java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,13 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println(" pemograman beoreantasi objek dengan java");
+
+        //contoh komentar 1 baris
+        /*
+       //contoh komentar lebih dari 1 baris
+         */
+        /**
+         * java doc-->baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
